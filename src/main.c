bc98345289e110c5e66b1d11cd138534bd439cf1
Enchan <enchant0130@gmail.com>
2024-01-14 12:57:03 +0900

    [Modify] lcnt blink

--- src/main.c
+++ src/main.c
@@ -22,22 +22,22 @@ void main(void) {
     // 表示の有効化
     display_setVisible(true);
 
-    // 描画バッファ取得+編集
+    // 描画バッファ取得
     volatile uint8_t* displayBuffer = display_getDrawBuffer();
-    displayBuffer[2] = 0x50;
-    displayBuffer[5] = 0x05;
 
     uint8_t lcnt = 0;
     while (true) {
+        // 一列目にループカウンタを表示
         displayBuffer[0] = lcnt;
 
-        if (lcnt % 64 == 0) {
-            displayBuffer[1] = 0xFF;
-        }
-        if (lcnt % 192 == 0) {
-            displayBuffer[1] = 0x00;
+        // lcntが16未満なら3列目を全点灯
+        if (lcnt >> 4 == 0) {
+            displayBuffer[2] = 0xFF;
+        } else {
+            displayBuffer[2] = 0x00;
         }
 
+        // カウンタは1msごとに増加
         lcnt++;
         __delay_ms(1);
     }
